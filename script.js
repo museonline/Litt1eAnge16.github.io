8b2471abf1352b63b9d51abdc6735df87d5887ac
Litt1eAnge16 <78031094+Litt1eAnge16@users.noreply.github.com>
2021-02-08 14:07:21 +0300

    111

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // при клике по кнопке скрипт запустит код
-button.onclick = function () {
+buttonlike.onclick = function () {
   // если фон кнопки чёрный
   if (button.style.backgroundColor == 'black') {
     // изменим его на белый, а текст сделаем чёрным
